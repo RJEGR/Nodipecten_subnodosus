61ef78754d8cd7d1c7d9aa5972a09309f72295b1
Ricardo Gore <rjhgore@gmail.com>
2023-12-01 16:57:58 -0800

    Update COUNT_MATRIX_HEATMAP.R

--- COUNT_MATRIX_HEATMAP.R
+++ COUNT_MATRIX_HEATMAP.R
@@ -79,18 +79,15 @@ library(ggh4x)
 P <- sample_cor_long %>%
   ggplot(aes(x = Library_ID, y = name, fill = cor)) +  
   geom_tile(linewidth = 0.2) +
-  ggsci::scale_fill_material(name = "", "blue-grey") +
-  ggsci::scale_color_material(name = "", "blue-grey") +
+  # ggsci::scale_fill_material(name = "", "blue-grey") +
+  # ggsci::scale_color_material(name = "", "blue-grey") +
   scale_x_discrete(position = 'bottom') +
   ggh4x::scale_y_dendrogram(hclust = hc_samples, position = "left", labels = NULL) +
   guides(y.sec = guide_axis_manual(labels = hc_order, label_size = 5, label_family = "GillSans")) +
-  # ggh4x::scale_x_dendrogram(hclust = hc_samples, position = "top", labels = NULL) +
   theme_bw(base_size = 7, base_family = "GillSans") +
   labs(x = '', y = '') +
   theme(
     legend.position = "bottom",
-    # axis.text.x = element_blank(),
-    # axis.ticks.x = element_blank(),
     axis.text.x = element_text(angle = 90, hjust = 1,vjust = 1, size = 5),
     strip.background = element_rect(fill = 'grey89', color = 'white'),
     panel.border = element_blank(),
@@ -124,13 +121,10 @@ TOPDF <- sample_cor_long %>%
 topplot <- TOPDF %>%
   ggplot(aes(y = y, x = Library_ID, color = Condition)) +
   geom_point(shape = 15, size = 2) +
-  geom_text(aes(label = Site),  vjust = -1, hjust = 0.5, size = 1.5, family =  "GillSans", color = "black") +
+  geom_text(aes(label = Site),  vjust = -2, hjust = 0.5, size = 1, family =  "GillSans", color = "black") +
   ggh4x::scale_x_dendrogram(hclust = hc_samples, position = 'top', labels = NULL) +
-  # ggh4x::guide_dendro()
-  # guides(x.sec = guide_axis_manual(labels = hc_order, label_size = 3.5)) +
   theme_bw(base_family = "GillSans", base_size = 10) +
-  # see::scale_color_pizza(name = "", reverse = T) +
-  # scale_color_manual("", values = c("#DADADA", "#D4DBC2")) + # "#4575b4", "#d73027"
+  see::scale_color_pizza(name = "", reverse = T) +
   theme(legend.position = 'top',
     panel.border = element_blank(), 
     plot.background = element_rect(fill='transparent', color = 'transparent'),
@@ -140,3 +134,11 @@ topplot <- TOPDF %>%
     axis.text.y = element_blank(),
     axis.title = element_blank(),
     panel.grid.major = element_blank()) 
+
+
+
+library(patchwork)
+
+psave <- topplot/ plot_spacer() /P + plot_layout(heights = c(0.6, -0.5, 5))
+
+ggsave(psave, filename = 'SAMPLE_HEATMAP.png', path = path, width = 4, height = 5, device = png, dpi = 300)
