f3a623140e4140af6c022a777995e890a29d8f74
Ricardo Gore <rjhgore@gmail.com>
2024-08-27 13:07:15 -0700

    Update 2_wgcna_hetmap.R

--- WGCNA/2_wgcna_hetmap.R
+++ WGCNA/2_wgcna_hetmap.R
@@ -10,9 +10,24 @@ if(!is.null(dev.list())) dev.off()
 
 dir <- "~/Documents/GitHub/Nodipecten_subnodosus/Results/04.Quantification/"
 
-datTraits <- list.files(dir, pattern = "Manifest.tsv", full.names = T) 
+.datTraits <- list.files(dir, pattern = "Manifest.tsv", full.names = T) 
 
-datTraits <- read_tsv(datTraits) %>% rename("LIBRARY_ID" = "Group2") %>% as("matrix")
+.datTraits <- read_tsv(.datTraits) %>% 
+  select_at(vars(starts_with("Group"))) %>%
+  rename("LIBRARY_ID" = "Group2", "Population" = "Group3", "Condition" = "Group4") %>%
+  mutate(datTraits = paste0(Population, "-", Condition))
+
+datTraits <- table(.datTraits$LIBRARY_ID, .datTraits$datTraits)
+
+# 
+# .datTraits <- .datTraits %>% 
+#   mutate(values_from = 1) %>%
+#   pivot_wider(names_from = LIBRARY_ID, values_from = values_from, values_fill = 0) 
+# 
+# datTraits <- .datTraits %>% 
+#   mutate_if(is.character, as.factor) %>% 
+#   mutate_if(is.factor, as.numeric) %>%
+#   data.frame(row.names = datTraits$LIBRARY_ID)
 
 dim(datExpr <- readRDS(paste0(dir, 'wgcna_datExpr.rds')))
 
@@ -35,13 +50,11 @@ data.frame(reads, bwmodules) %>%
   summarise(n = n(), reads = sum(reads)) %>%
   dplyr::rename('module' = 'bwmodules') -> stats
 
-sum(rownames(datExpr) %in% datTraits[,5])
 
-rownames(datTraits) <- datTraits[,5]
+as_tibble(bwmodules, rownames = "Transcript") %>% 
+  dplyr::rename("WGCNA" = "value") %>% 
+  write_tsv(file = paste0(dir, "WGCNA.tsv"))
 
-datTraits <- datTraits[match(rownames(datTraits), rownames(MEs)),]
-
-identical(rownames(datTraits), rownames(MEs))
 
 # Recalculate MEs with color labeLIBRARY_ID# Recalculate MEs with color labels
 
@@ -49,16 +62,17 @@ MEs0 = moduleEigengenes(datExpr, bwmodules)$eigengenes
 
 MEs = orderMEs(MEs0)
 
-# rownames(MEs) <- str_replace_all(rownames(MEs), '^ME', '')
 names(MEs) <- str_replace_all(names(MEs), '^ME', '')
+
+identical(sort(rownames(datTraits)), sort(rownames(MEs)))
+
+# datTraits <- datTraits[match(rownames(datTraits), rownames(MEs)),]
+
+# identical(rownames(datTraits), rownames(MEs))
 
 moduleTraitCor = cor(MEs, datTraits, use= "p")
 
-MEs[is.na(MEs)] <- 0 
-
-
 moduleTraitPvalue = corPvalueStudent(moduleTraitCor, nrow(datTraits))
-
 
 moduleTraitCor %>% as_tibble(rownames = 'module') %>% 
   pivot_longer(-module, values_to = 'moduleTraitCor') -> df1
@@ -69,101 +83,96 @@ moduleTraitPvalue %>% as_tibble(rownames = 'module') %>%
 
 hclust <- hclust(dist(moduleTraitCor), "complete")
 
-up_df %>% distinct(transcript) %>% pull() -> updegs
-down_df %>% distinct(transcript) %>% pull() -> dwndegs
+# up_df %>% distinct(transcript) %>% pull() -> updegs
+# down_df %>% distinct(transcript) %>% pull() -> dwndegs
 
 bwmodules %>% 
   as_tibble(., rownames = 'transcript') %>%
-  mutate(degs = ifelse(transcript %in% updegs, 'up', 
-    ifelse(transcript %in% dwndegs, 'down', ''))) %>%
+  # mutate(degs = ifelse(transcript %in% updegs, 'up', 
+    # ifelse(transcript %in% dwndegs, 'down', ''))) %>%
   dplyr::rename('module' = 'value') -> bwModuleCol
 
 bwModuleCol %>%  group_by(module) %>% count(sort = T) %>% left_join(stats)
 
 bwModuleCol %>% 
-  group_by(module, degs) %>% count(sort = T) -> bwModuleDF
+  group_by(module) %>% count(sort = T) -> bwModuleDF
 
 bwModuleDF %>% mutate(module = factor(module, levels = hclust$labels[hclust$order])) -> bwModuleDF
 
 bwModuleDF %>% group_by(module) %>% mutate(pct = n / sum(n)) -> bwModuleDF
-
-
 
 df1 %>%
   mutate(star = ifelse(corPvalueStudent <.001, "***", 
     ifelse(corPvalueStudent <.01, "**",
       ifelse(corPvalueStudent <.05, "*", "")))) -> df1
 
+
+df1 <- df1 %>% separate(name, into = c("Population", "Assay"), sep = "-", remove = F)
+
+lo = floor(min(df1$moduleTraitCor))
+up = ceiling(max(df1$moduleTraitCor))
+mid = (lo + up)/2
+
+
+# recode_to <- c("Desarrollo", "Crecimiento", "Calcificación", "Respiración")
+# recode_to <- structure(recode_to,names = colnames(datTraits))
+
+assay_levs <- c("F0", "F1", "Basal", "Regular", "Caotica", "Constante")
+
+y_labels <- bwModuleDF %>% mutate(label = paste0(module, " (", n,")")) %>% pull(label, name = module)
+
+# library(ggh4x)
+
 df1 %>%
-  mutate(name = factor(name, levels = c('Boquita', 'Carrizales', 'Green', 'Brown'))) %>%
-  mutate(facet = ifelse(name %in% c('Boquita', 'Carrizales'), 'Site', 'Morphotype')) %>%
+  mutate(Assay = factor(Assay, levels = assay_levs)) %>%
+  # mutate(name = recode_factor(name, !!!recode_to, .ordered = T)) %>%
   mutate(moduleTraitCor = round(moduleTraitCor, 2)) %>%
-  # mutate(star = ifelse(star != '', paste0(moduleTraitCor, '(', star,')'), moduleTraitCor)) %>%
   mutate(star = ifelse(star != '', paste0(moduleTraitCor, '(', star,')'), '')) %>%
-  ggplot(aes(y = module, x = name, fill = moduleTraitCor)) +
+  ggplot(aes(y = module, x = Assay, fill = moduleTraitCor)) +
+  ggh4x::facet_nested(~ Population, scales = "free") +
+  # facet_grid(~ Population+Condition, scales = "free")
   # geom_tile(color = 'black', size = 0.5, width = 0.7) + 
   geom_raster() +
-  geom_text(aes(label = star),  vjust = 0.5, hjust = 0.5, size= 5, family =  "GillSans") +
-  ggsci::scale_fill_gsea(name = "", reverse = T, na.value = "white") +
-  # scale_fill_viridis_c(name = "Membership", na.value = "white") +
-  ggh4x::scale_y_dendrogram(hclust = hclust) +
+  geom_text(aes(label = star),  vjust = 0.5, hjust = 0.5, size= 1.5, family =  "GillSans") +
+  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
+    na.value = "white", midpoint = mid, limit = c(lo, up),
+    name = NULL) +
+  ggh4x::scale_y_dendrogram(hclust = hclust, labels = NULL) +
   labs(x = '', y = 'Module') +
-  guides(fill = guide_colorbar(barwidth = unit(3, "in"),
+  guides(
+    fill = guide_colorbar(barwidth = unit(3, "in"),
+    barheight = unit(0.1, "in"), label.position = "bottom",
     alignd = 0.5,
-    ticks.colour = "black", ticks.linewidth = 0.5,
-    frame.colour = "black", frame.linewidth = 0.5,
-    label.theme = element_text(size = 12))) +
+    title = "Trait correlation",
+    title.position  = "top",
+    title.theme = element_text(size = 10, family = "GillSans", hjust = 1),
+    ticks.colour = "black", ticks.linewidth = 0.35,
+    frame.colour = "black", frame.linewidth = 0.35,
+    label.theme = element_text(size = 10, family = "GillSans")),
+    
+    y.sec = ggh4x::guide_axis_manual(labels = y_labels, label_size = 8, label_family = "GillSans")
+    
+    ) +
   theme_classic(base_size = 12, base_family = "GillSans") +
   theme(legend.position = "top",
     strip.background = element_rect(fill = 'white', color = 'white'),
+    strip.text = element_text(color = "black",hjust = 0, size = 7),
     axis.line.y = element_line(color = 'white'),
-    axis.text.y = element_text(hjust = 1.2),
-    axis.ticks.length = unit(5, "pt")) +
-    facet_wrap(~ name, scales = 'free_x') -> p1 
+    axis.text.y = element_text(hjust = 1),
+    axis.ticks.length = unit(5, "pt")) -> p1 
 
-p1 <- p1 + theme(axis.ticks.x = element_blank(), axis.text.x = element_blank(), axis.line.x = element_blank())
-
+p1 <- p1 + theme(axis.ticks.x = element_blank(), 
+  axis.text.x = element_text(angle = 90, hjust = 1, size = 10),
+  # axis.text.x = element_blank(), 
+  axis.line.x = element_blank())
+# 
 p1 <- p1 + theme(panel.spacing.x = unit(0, "mm"))
 
-ggsave(p1, filename = 'ModuleTraitRelationship_1.png', 
-  path = path, width = 5, height = 5, dpi = 1000)
+# p1
 
+ggsave(p1, filename = 'ModuleTraitRelationship.png', 
+  path = dir, width = 5, height = 10, dpi = 1000, device = png)
 
-# negative lFC == Carrizales
-# Positive lFC == Boquita
-
-
-bwModuleDF %>% 
-  # mutate(degs = ifelse(degs == 'ns', '', degs)) %>%
-  ggplot(aes(x = module, y = n, fill = degs)) + 
-  labs(y = 'Number of transcripts') +
-  geom_col() + coord_flip() +
-  # geom_col(color = 'black', size = 0.25) + coord_flip() +
-  scale_fill_manual(name = '', values = c("grey30", "#EE4141", "#2428D0")) +
-  theme_classic(base_size = 12, base_family = "GillSans") +
-  theme(legend.position = "top",
-    axis.title.y = element_blank(), axis.text.y= element_blank(),
-    axis.ticks.y=element_blank(), axis.line.y = element_blank()) -> p2
-
-library(patchwork)
-
-p1 + p2 + plot_layout(widths = c(1, 0.5)) +
-  labs(caption = '* corPvalueStudent < 0.05 ') -> psave
-
-ggsave(psave, filename = 'ModuleTraitRelationship_2.png', path = path, width = 6.7, height = 5,dpi = 1000)
-
-# set significant modules
-
-df1 %>% filter(corPvalueStudent < 0.05) %>% pull(module) %>% unique() -> psME
-
-bwModuleCol %>% filter(module %in% psME) -> pbwModuleCol
-
-# get relevant genes
-
-head(bwmodules_sig <- bwmodules[bwmodules %in% psME])
-
-save(moduleTraitCor, moduleTraitPvalue, MEs, psME, datTraits, df1, bwModuleDF, bwModuleCol,hclust,
-  file = paste0(path, 'moduleTraitRelationship.RData'))
 
 
 # Intramodular connectivity ----
@@ -203,8 +212,18 @@ geneTraitSignificance <- as.data.frame(cor(datExpr,datTraits, use = "p"))
 
 # What are the p-values for each correlation?
 GSPvalue <- as.data.frame(corPvalueStudent(as.matrix(geneTraitSignificance), nSamples = nrow(datExpr)))
-names(geneTraitSignificance) <- paste("GS.", names(datTraits), sep = "")
-names(GSPvalue) <- paste("p.GS.", names(datTraits), sep = "")
+names(geneTraitSignificance) <- paste("GS.", colnames(datTraits), sep = "")
+names(GSPvalue) <- paste("p.GS.", colnames(datTraits), sep = "")
+
+
+identical(rownames(geneTraitSignificance), rownames(GSPvalue))
+
+bind <- cbind(geneTraitSignificance, GSPvalue) %>% as_tibble(rownames = "Transcript")
+
+as_tibble(bwmodules, rownames = "Transcript") %>% 
+  dplyr::rename("WGCNA" = "value") %>% 
+  left_join(bind) %>% 
+  write_tsv(file = paste0(dir, "GeneTraitSignificance.tsv"))
 
 
 GSpval <- GSPvalue %>%
@@ -238,7 +257,7 @@ allData_df <- gMM_df %>%
 bwmodules %>% as_tibble(rownames = 'ids') %>% rename('module' = 'value') %>%
   left_join(allData_df) -> allData_df
 
-saveRDS(allData_df, file = paste0(path,'allData_df.rds'))
+saveRDS(allData_df, file = paste0(dir,'allData_df.rds'))
 
 # Sanity check
 allData_df %>% group_by(module) %>% count() 
@@ -265,6 +284,26 @@ allData_df %>%
 
 plotscatterSig(df_viz, M = psME)
 
+colVal <- c("#54278f", "grey30")
+
+df_viz %>%
+  mutate(color = ifelse(p.GS < 0.05, '*', 'ns')) %>%
+  filter(module %in% "blue") %>%
+  # filter(group != 'HC') %>%
+  ggplot(aes(x = moduleMemberCorr, y = abs(GS), color = color, alpha = -log10(p.GS))) +  
+  # geom_vline(color = "black", xintercept = 0.8, linetype="dashed", alpha=0.5) +
+  # geom_hline(color = "black", yintercept = 0.8, linetype="dashed", alpha=0.5) +
+  # geom_hline(color = "black", yintercept = -0.8, linetype="dashed", alpha=0.5) +
+  geom_point(size = 1) +
+  labs(x = 'Module membership', y = 'Gene significance') + # 'Gene Correlation'
+  scale_color_manual(name = '', values = colVal) + # breaks = c(0, 0.25 ,0.5, 0.75, 1), limits= c(0, 1)
+  scale_alpha_continuous(name = expression(-Log[10] ~ "P")) +
+  facet_grid(module~group) +
+  # ggh4x::facet_nested_wrap(module+group ~., nest_line = T) +
+  theme_bw(base_family = "GillSans", base_size = 16) +
+  # geom_smooth(method = "lm", linetype="dashed", size = 0.5, alpha=0.5, color = 'white',
+  #   se = TRUE, na.rm = TRUE) +
+  theme(legend.position = 'top') -> psave
 
 allData_df %>%
   ggplot(aes(moduleMemberCorr)) +
