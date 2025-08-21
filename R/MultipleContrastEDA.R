87a7380bae9c60ce1d713036b0e53e5525325b91
Ricardo Gore <rjhgore@gmail.com>
2025-08-21 12:44:18 -0700

    Update MultipleContrastEDA.R

--- R/MultipleContrastEDA.R
+++ R/MultipleContrastEDA.R
@@ -22,118 +22,128 @@ datExpr <- round(datExpr)
 
 .colData <- list.files(dir, pattern = "metadata_experimental.tsv", full.names = T) 
 
-.colData <- read_tsv(.colData)
+.colData <- read_tsv(.colData) %>% filter(Condition != "Reg")
 
 table(.colData$Site, .colData$Condition)
 
-.colData <- .colData %>% mutate(design = paste(Site, Condition, sep = "_")) %>%
+.colData <- 
+  .colData %>% mutate(sample_pairs = paste(Site, Condition, sep = "_")) %>%
   mutate_if(is.character, as.factor)
 
 
-g <- levels(.colData$design)
-
-# heatmap(model.matrix(~g), keep.dendro = F)
-
-create_pairs <- function(vec) {
-  combn(vec, 2, simplify = FALSE)
+run_DESEQ <- function(count, contrast_col, colData) {
+  
+  names(colData)[names(colData) %in% "design"] <- "drop_duplicate_col"
+  
+  colData <- colData %>% dplyr::rename("design" = contrast_col)
+  
+  g <- levels(colData$design)
+  
+  create_pairs <- function(vec) {
+    combn(vec, 2, simplify = FALSE)
+  }
+  
+  
+  combinations_vector <- unlist(lapply( create_pairs(g), paste, collapse = "-"))
+  
+  # Print the combinations vector
+  print(combinations_vector)
+  
+  gstr <- strsplit(combinations_vector, "-")
+  
+  
+  run_analysis <- function(count, gstr, colData) {
+    
+    cat("\nContrasting: ", gstr, "\n")
+    
+    colData <- colData %>% filter(design %in% gstr)
+    
+    sam_gstr <- structure(
+      as.character(colData$design), 
+      names = as.character(colData$Library_ID))
+    
+    sam_gstr <- sam_gstr[sam_gstr %in% gstr]
+    
+    fl <- as.factor(sam_gstr)
+    
+    colData <- colData %>% mutate(design = relevel(design, ref = levels(fl)[1]))
+    
+    
+    # design <- model.matrix(~fl)
+    
+    # filter samples from the contrast
+    
+    keep_cols <- colnames(count) %in% names(sam_gstr)
+    
+    count <- round(count[,keep_cols])
+    
+    by_count <- 1; by_freq <- 1
+    
+    keep_genes <- rowSums(count > by_count) >= by_freq
+    
+    cat("\nUsing N genes: ", sum(keep_genes),"\n")
+    
+    cat("\nUsing samples: ", colnames(count),"\n")
+    
+    # dim(count <- count[keep_genes, keep_cols ])
+    
+    colnames(count) <- sam_gstr
+    
+    count <- as(count, 'matrix')
+    
+    count <- round(count)
+    require(DESeq2)
+    
+    ddsFullCountTable <- DESeqDataSetFromMatrix(
+      countData = count,
+      colData = colData,
+      design = ~ design)
+    
+    dds <- DESeq(ddsFullCountTable)
+    
+    result_table = results(dds)
+    
+    
+    sampleA <- levels(fl)[1]
+    sampleB <- levels(fl)[2]
+    
+    result_table <- data.frame(sampleA,
+      sampleB,
+      result_table)
+    
+    
+    reorder_cols <- c( "ids" ,"sampleA", "sampleB", "log2FoldChange",  "pvalue", "padj")
+    
+    result_table %>%
+      as_tibble(rownames = 'ids') %>%
+      mutate_at(vars(!matches("ids|sample|PValue|FDR")),
+        round ,digits = 2) %>%
+      select_at(vars(all_of(reorder_cols)))
+  }
+  
+  OUT <- lapply(gstr, function(x) run_analysis(count, gstr = x, colData))
+  
+  do.call(rbind, OUT) %>% filter(padj < 0.05)
+  
 }
 
 
-combinations_vector <- unlist(lapply( create_pairs(g), paste, collapse = "-"))
-
-# Print the combinations vector
-print(combinations_vector)
-
-# lapply(combinations_vector, function(x) unlist(strsplit(x, "-")))
-
-# continue here
-
-run_DESEQ <- function(count, gstr, colData) {
-  
-  cat("\nContrasting: ", gstr, "\n")
-  
-  colData <- colData %>% filter(design %in% gstr)
-  
-  sam_gstr <- structure(
-    as.character(colData$design), 
-    names = as.character(colData$Library_ID))
-  
-  sam_gstr <- sam_gstr[sam_gstr %in% gstr]
-  
-  fl <- as.factor(sam_gstr)
-  
-  colData <- colData %>% mutate(design = relevel(design, ref = levels(fl)[1]))
-  
-  
-  # design <- model.matrix(~fl)
-  
-  # filter samples from the contrast
-  
-  keep_cols <- colnames(count) %in% names(sam_gstr)
-  
-  count <- round(count[,keep_cols])
-  
-  by_count <- 1; by_freq <- 1
-  
-  keep_genes <- rowSums(count > by_count) >= by_freq
-  
-  cat("\nUsing N genes: ", sum(keep_genes),"\n")
-  
-  cat("\nUsing samples: ", colnames(count),"\n")
-  
-  # dim(count <- count[keep_genes, keep_cols ])
-  
-  colnames(count) <- sam_gstr
-  
-  count <- as(count, 'matrix')
-  
-  count <- round(count)
-  
-  require(DESeq2)
-  
-  ddsFullCountTable <- DESeqDataSetFromMatrix(
-    countData = count,
-    colData = colData,
-    design = ~ design)
-  
-  dds <- DESeq(ddsFullCountTable)
-  
-  # result_table <- tTags$table
-  result_table = results(dds)
-
-  
-  sampleA <- levels(fl)[1]
-  sampleB <- levels(fl)[2] 
-  
-  result_table <- data.frame(sampleA,
-    sampleB, 
-    result_table)
-  
-  
-  reorder_cols <- c( "ids" ,"sampleA", "sampleB", "log2FoldChange",  "pvalue", "padj")
-  
-  result_table %>%
-    as_tibble(rownames = 'ids') %>%
-    # mutate(logFC = -1 * logFC) %>% # reset logfc so it's A/B instead of B/A to be consistent with DESeq2
-    mutate_at(vars(!matches("ids|sample|PValue|FDR")),
-      round ,digits = 2) %>%
-    select_at(vars(all_of(reorder_cols)))
-  
-}
-
-comb_list <- strsplit(combinations_vector, "-")
-# 
-# run_DESEQ(head(datExpr, 100),
-#   gstr = comb_list[[1]], .colData) %>%
-#   dplyr::count(sampleA, sampleB)
-
-OUT <- lapply(comb_list, 
-  function(x) run_DESEQ(datExpr, gstr = x, .colData))
-
-OUT <- do.call(rbind, OUT)
-
-OUT <- OUT %>% filter(padj < 0.05)
-
-OUT %>% dplyr::count(sampleA, sampleB) 
+OUT <- run_DESEQ(datExpr, contrast_col = "sample_pairs", colData = .colData)
 
 write_rds(OUT, file = paste0(file.path(dir, subdir), "/p05_DESEQ_multiple_contrast.rds"))
+
+# Multiple contrast by Site (Evals si localidad tiene un efecto en el estres)
+
+
+OUT_condition <- run_DESEQ(datExpr, contrast_col = "Condition", colData = .colData)
+
+OUT_condition %>% dplyr::count(sampleA, sampleB) 
+
+write_rds(OUT_condition, file = paste0(file.path(dir, subdir), "/p05_DESEQ_multiple_contrast_condition.rds"))
+
+# .colData %>% dplyr::count(Site)
+
+# OUT_site <- run_DESEQ(datExpr, contrast_col = "Site", colData = .colData)
+
+# write_rds(OUT_site, file = paste0(file.path(dir, subdir), "/p05_DESEQ_multiple_contrast_site.rds"))
+
